1be4146f80e855175f38007f29d2a0fea4b8473e
ImeshGu <imeshguna3@gmail.com>
2023-10-20 11:43:36 +0530

    minorCorrection

--- Arduino/sketch_sensor_reading/sketch_sensor_reading.ino
+++ Arduino/sketch_sensor_reading/sketch_sensor_reading.ino
@@ -23,7 +23,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello Worlds");
+  Serial.println("Hello Worlds");     
 
   float shuntvoltage = 0;
   float busvoltage = 0;
